5ba0860f7572241aab550dd8460735241b4a76f3
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2021-05-10 11:53:29 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #f7f0cd;
+                background-color: #ffffde;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
