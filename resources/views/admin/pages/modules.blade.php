c79cdcc88c0e7fef45191c9aa03289842e967d4c
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2021-05-18 10:46:36 +0300

    fixes

--- resources/views/admin/pages/modules.blade.php
+++ resources/views/admin/pages/modules.blade.php
@@ -1,52 +1,61 @@
 @extends('admin.main')
 <div id="main">
     @include('admin/_partials/sidebar')
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <div class="page-heading">
         <h3>Modulių valdymas</h3>
     </div>
     <div class="card">
-  <div class="card-body">
-    <blockquote class="blockquote mb-0">
-    <form method="post" action="/createModule">
-    {{csrf_field()}}
-        <div class="page-heading">
-            <h4>Pridėti modulį</h4>
-        </div>
-        <div class="form row">
-            <div class="form-group col-md-6">
-                <label for="name">Mokytojo Vardas</label>
-                <input type="text" class="form-control" placeholder="Vardas" required>
-            </div>
-        <div class="form-group col-md-6">
-            <label for="surname">Mokytojo Pavardė</label>
-            <input type="text" class="form-control" placeholder="Pavardė" required>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <form method="post" action="/createModule">
+                    {{csrf_field()}}
+                    <div class="page-heading">
+                        <h4>Pridėti modulį</h4>
+                    </div>
+                    <div class="form row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Mokytojo Vardas</label>
+                            <input type="text" class="form-control" placeholder="Vardas" name="name">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="surname">Mokytojo Pavardė</label>
+                            <input type="text" class="form-control" placeholder="Pavardė" name="surname">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="groupName">Grupės pavadinimas</label>
+                        <input type="text" class="form-control" placeholder="Grupės pavadinimas" name="groupName">
+                    </div>
+                    <div class="form-group">
+                        <label for="moduleName">Modulio pavadinimas</label>
+                        <input type="text" class="form-control" placeholder="Modulio pavadinimas" name="moduleName">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Pateikti</button>
+                </form>
+            </blockquote>
         </div>
     </div>
-  <div class="form-group">
-    <label for="groupName">Grupės pavadinimas</label>
-    <input type="text" class="form-control" placeholder="Grupės pavadinimas" required>
-  </div>
-  <div class="form-group">
-    <label for="moduleName">Modulio pavadinimas</label>
-    <input type="text" class="form-control" placeholder="Modulio pavadinimas"required>
-  </div>
-  <button type="submit" class="btn btn-primary">Pateikti</button>
-</form>
-    </blockquote>
-  </div>
-</div>
     <div class="box">
         @foreach($modules as $module)
-            <div class="card" style="width: 13rem; height: 13rem">
-                <div class="card-body d-flex flex-column align-items-center justify-content-center">
-                    <h5 class="card-title">{{$module->module_name}}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{$module->teacher_name}}</h6>
-                    <h6 class="card-subtitle mb-2 text-muted">{{$module->teacher_surname}}</h6>
-                    <h6 class="card-subtitle">{{$module->group_name}}</h6>
-                    <a href="/updateModule/{{$module->id}}">redaguoti</a>
-                    <a href="/deleteModule/{{$module->id}}">pasalinti</a>
-                </div>
+        <div class="card" style="width: 13rem; height: 13rem">
+            <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <h5 class="card-title">{{$module->module_name}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{$module->teacher_name}}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">{{$module->teacher_surname}}</h6>
+                <h6 class="card-subtitle">{{$module->group_name}}</h6>
+                <a href="/updateModule/{{$module->id}}">redaguoti</a>
+                <a href="/deleteModule/{{$module->id}}">pasalinti</a>
             </div>
+        </div>
         @endforeach
     </div>
 </div>
